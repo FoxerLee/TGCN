1369647b8c6d4bdffe2343f5e3c154b717fe596c
foxerlee <foxerlee1@gmail.com>
2020-11-18 15:19:58 -0500

    finish tf2 change

--- config.py
+++ config.py
@@ -1,19 +1,34 @@
 
 
-class CONFIG(object):
-    """docstring for CONFIG"""
-    def __init__(self):
-        super(CONFIG, self).__init__()
+# class CONFIG(object):
+#     """docstring for CONFIG"""
+#     def __init__(self):
+#         super(CONFIG, self).__init__()
         
-        self.dataset = 'R8'
-        self.model = 'gcn'  # 'gcn', 'gcn_cheby', 'dense'
-        self.learning_rate = 0.02   # Initial learning rate.
-        self.epochs  = 200  # Number of epochs to train.
-        self.hidden1 = 200  # Number of units in hidden layer 1.
-        self.dropout = 0.5  # Dropout rate (1 - keep probability).
-        self.weight_decay = 0.   # Weight for L2 loss on embedding matrix.
-        self.early_stopping = 10 # Tolerance for early stopping (# of epochs).
-        self.max_degree = 3      # Maximum Chebyshev polynomial degree.
+#         self.dataset = 'R8'
+#         self.model = 'gcn'  # 'gcn', 'gcn_cheby', 'dense'
+#         self.learning_rate = 0.02   # Initial learning rate.
+#         self.epochs  = 200  # Number of epochs to train.
+#         self.hidden1 = 200  # Number of units in hidden layer 1.
+#         self.dropout = 0.5  # Dropout rate (1 - keep probability).
+#         self.weight_decay = 0.   # Weight for L2 loss on embedding matrix.
+#         self.early_stopping = 10 # Tolerance for early stopping (# of epochs).
+#         self.max_degree = 3      # Maximum Chebyshev polynomial degree.
 
 
+import  argparse
 
+args = argparse.ArgumentParser()
+args.add_argument('--dataset', default='cora')
+args.add_argument('--model', default='gcn')
+args.add_argument('--learning_rate', default=0.01)
+args.add_argument('--epochs', default=200)
+args.add_argument('--hidden1', default=16)
+args.add_argument('--dropout', default=0.5)
+args.add_argument('--weight_decay', default=5e-4)
+args.add_argument('--early_stopping', default=10)
+args.add_argument('--max_degree', default=3)
+
+
+args = args.parse_args()
+print(args)
--- train.py
+++ train.py
@@ -1,172 +1,214 @@
-from __future__ import division
-from __future__ import print_function
-from sklearn import metrics
-import random
-import time
-import sys
-import os
+# from __future__ import division
+# from __future__ import print_function
+# from sklearn import metrics
+# import random
+# import time
+# import sys
+# import os
 
-import torch
-import torch.nn as nn
+# import torch
+# import torch.nn as nn
 
 import numpy as np
 
-from utils.utils import *
-from models.gcn import GCN
-from models.mlp import MLP
+# from utils.utils import *
+# from models.gcn import GCN
+# from models.mlp import MLP
 
-from config import CONFIG
-cfg = CONFIG()
+# from config import CONFIG
+# cfg = CONFIG()
+
+import  time
+import  tensorflow as tf
+from    tensorflow.keras import optimizers
+
+from    utils.utils import *
+from    models import GCN, MLP
+from    config import args
+
+import  os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+print('tf version:', tf.__version__)
+assert tf.__version__.startswith('2.')
 
 
-if len(sys.argv) != 2:
-	sys.exit("Use: python train.py <dataset>")
+# if len(sys.argv) != 2:
+	# sys.exit("Use: python train.py <dataset>")
 
-# datasets = ['20ng', 'R8', 'R52', 'ohsumed', 'mr']
-dataset = sys.argv[1]
+# dataset = sys.argv[1]
 
 # if dataset not in datasets:
 	# sys.exit("wrong dataset name")
-cfg.dataset = dataset
+# cfg.dataset = dataset
 
-# Set random seed
-seed = random.randint(1, 200)
-seed = 2019
+# set random seed
+seed = 123
 np.random.seed(seed)
-torch.manual_seed(seed)
-if torch.cuda.is_available():
-    device = torch.device("cuda")
-    print('Using GPU ', torch.cuda.get_device_name(0)) 
+tf.random.set_seed(seed)
 
-
-# Settings
-# os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 # Load data
-adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask, train_size, test_size = load_corpus(
-    cfg.dataset)
+adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask, train_size, test_size = load_corpus(args.dataset)
+
 
 features = sp.identity(features.shape[0])  # featureless
-
-
-# Some preprocessing
 features = preprocess_features(features)
-if cfg.model == 'gcn':
+
+
+
+if args.model == 'gcn':
     support = [preprocess_adj(adj)]
     num_supports = 1
     model_func = GCN
-elif cfg.model == 'gcn_cheby':
-    support = chebyshev_polynomials(adj, cfg.max_degree)
-    num_supports = 1 + cfg.max_degree
+elif args.model == 'gcn_cheby':
+    support = chebyshev_polynomials(adj, args.max_degree)
+    num_supports = 1 + args.max_degree
     model_func = GCN
-elif cfg.model == 'dense':
+elif args.model == 'dense':
     support = [preprocess_adj(adj)]  # Not used
     num_supports = 1
     model_func = MLP
 else:
-    raise ValueError('Invalid argument for model: ' + str(cfg.model))
+    raise ValueError('Invalid argument for model: ' + str(args.model))
 
 
-# Define placeholders
-t_features = torch.from_numpy(features)
-t_y_train = torch.from_numpy(y_train)
-t_y_val = torch.from_numpy(y_val)
-t_y_test = torch.from_numpy(y_test)
-t_train_mask = torch.from_numpy(train_mask.astype(np.float32))
-tm_train_mask = torch.transpose(torch.unsqueeze(t_train_mask, 0), 1, 0).repeat(1, y_train.shape[1])
+
+t_features = tf.convert_to_tensor(features)
+t_y_train = tf.convert_to_tensor(y_train)
+t_y_val = tf.convert_to_tensor(y_val)
+t_y_test = tf.convert_to_tensor(y_test)
+t_train_mask = tf.convert_to_tensor(train_mask.astype(np.float32))
+tm_train_mask = tf.tile(tf.transpose(tf.expand_dims(t_train_mask, 0), perm=[1, 0]), [1, y_train.shape[1]])
+
+
+
+# # Define placeholders
+# t_features = torch.from_numpy(features)
+# t_y_train = torch.from_numpy(y_train)
+# t_y_val = torch.from_numpy(y_val)
+# t_y_test = torch.from_numpy(y_test)
+# t_train_mask = torch.from_numpy(train_mask.astype(np.float32))
+# tm_train_mask = torch.transpose(torch.unsqueeze(t_train_mask, 0), 1, 0).repeat(1, y_train.shape[1])
 
 t_support = []
 for i in range(len(support)):
-    t_support.append(torch.Tensor(support[i]))
+    t_support.append(tf.convert_to_tensor(support[i]))
 
-if torch.cuda.is_available():
-    # model_func = model_func.to(device)
-    t_features = t_features.to(device)
-    t_y_train = t_y_train.to(device)
-    t_y_val = t_y_val.to(device)
-    t_y_test = t_y_test.to(device)
-    t_train_mask = t_train_mask.to(device)
-    tm_train_mask = tm_train_mask.to(device)
-    for i in range(len(support)):
-        t_support = [t.to(device) for t in t_support if True]
+# if torch.cuda.is_available():
+#     # model_func = model_func.to(device)
+#     t_features = t_features.to(device)
+#     t_y_train = t_y_train.to(device)
+#     t_y_val = t_y_val.to(device)
+#     t_y_test = t_y_test.to(device)
+#     t_train_mask = t_train_mask.to(device)
+#     tm_train_mask = tm_train_mask.to(device)
+#     for i in range(len(support)):
+#         t_support = [t.to(device) for t in t_support if True]
         
-model = model_func(input_dim=features.shape[0], support=t_support, num_classes=y_train.shape[1])
-model.to(device)
+# model = model_func(input_dim=features.shape[0], support=t_support, num_classes=y_train.shape[1])
+# model.to(device)
+
+# Create model
+model = GCN(input_dim=features.shape[0], output_dim=y_train.shape[1], num_features_nonzero=features[1].shape) # [1433]
+
+
+
+
 
 # Loss and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=cfg.learning_rate)
+optimizer = optimizers.Adam(lr=args.learning_rate)
 
+
+for epoch in range(args.epochs):
+
+    with tf.GradientTape() as tape:
+        loss, acc = model((t_features, t_y_train, tm_train_mask, t_support))
+    grads = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+    _, val_acc = model((t_features, t_y_val, val_mask, t_support), training=False)
+
+
+    if epoch % 20 == 0:
+
+        print(epoch, float(loss), float(acc), '\tval:', float(val_acc))
+
+
+
+test_loss, test_acc = model((features, test_label, test_mask, support), training=False)
+
+
+print('\ttest:', float(test_loss), float(test_acc))
 
 # Define model evaluation function
-def evaluate(features, labels, mask):
-    t_test = time.time()
-    # feed_dict_val = construct_feed_dict(
-    #     features, support, labels, mask, placeholders)
-    # outs_val = sess.run([model.loss, model.accuracy, model.pred, model.labels], feed_dict=feed_dict_val)
-    model.eval()
-    with torch.no_grad():
-        logits = model(features)
-        t_mask = torch.from_numpy(np.array(mask*1., dtype=np.float32)).to(device)
-        tm_mask = torch.transpose(torch.unsqueeze(t_mask, 0), 1, 0).repeat(1, labels.shape[1]).to(device)
-        loss = criterion(logits * tm_mask, torch.max(labels, 1)[1])
-        pred = torch.max(logits, 1)[1]
-        acc = ((pred == torch.max(labels, 1)[1]).float() * t_mask).sum().item() / t_mask.sum().item()
+# def evaluate(features, labels, mask):
+#     t_test = time.time()
+#     # feed_dict_val = construct_feed_dict(
+#     #     features, support, labels, mask, placeholders)
+#     # outs_val = sess.run([model.loss, model.accuracy, model.pred, model.labels], feed_dict=feed_dict_val)
+#     model.eval()
+#     with torch.no_grad():
+#         logits = model(features)
+#         t_mask = torch.from_numpy(np.array(mask*1., dtype=np.float32)).to(device)
+#         tm_mask = torch.transpose(torch.unsqueeze(t_mask, 0), 1, 0).repeat(1, labels.shape[1]).to(device)
+#         loss = criterion(logits * tm_mask, torch.max(labels, 1)[1])
+#         pred = torch.max(logits, 1)[1]
+#         acc = ((pred == torch.max(labels, 1)[1]).float() * t_mask).sum().item() / t_mask.sum().item()
         
-    return loss.cpu().numpy(), acc, pred.cpu().numpy(), labels.cpu().numpy(), (time.time() - t_test)
+#     return loss.cpu().numpy(), acc, pred.cpu().numpy(), labels.cpu().numpy(), (time.time() - t_test)
 
 
 
-val_losses = []
+# val_losses = []
 
-# Train model
-for epoch in range(cfg.epochs):
+# # Train model
+# for epoch in range(cfg.epochs):
 
-    t = time.time()
+#     t = time.time()
     
-    # Forward pass
-    logits = model(t_features)
-    loss = criterion(logits * tm_train_mask, torch.max(t_y_train, 1)[1])    
-    acc = ((torch.max(logits, 1)[1] == torch.max(t_y_train, 1)[1]).float() * t_train_mask).sum().item() / t_train_mask.sum().item()
+#     # Forward pass
+#     logits = model(t_features)
+#     loss = criterion(logits * tm_train_mask, torch.max(t_y_train, 1)[1])    
+#     acc = ((torch.max(logits, 1)[1] == torch.max(t_y_train, 1)[1]).float() * t_train_mask).sum().item() / t_train_mask.sum().item()
         
-    # Backward and optimize
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+#     # Backward and optimize
+#     optimizer.zero_grad()
+#     loss.backward()
+#     optimizer.step()
 
-    # Validation
-    val_loss, val_acc, pred, labels, duration = evaluate(t_features, t_y_val, val_mask)
-    val_losses.append(val_loss)
+#     # Validation
+#     val_loss, val_acc, pred, labels, duration = evaluate(t_features, t_y_val, val_mask)
+#     val_losses.append(val_loss)
 
-    print_log("Epoch: {:.0f}, train_loss= {:.5f}, train_acc= {:.5f}, val_loss= {:.5f}, val_acc= {:.5f}, time= {:.5f}"\
-                .format(epoch + 1, loss, acc, val_loss, val_acc, time.time() - t))
+#     print_log("Epoch: {:.0f}, train_loss= {:.5f}, train_acc= {:.5f}, val_loss= {:.5f}, val_acc= {:.5f}, time= {:.5f}"\
+#                 .format(epoch + 1, loss, acc, val_loss, val_acc, time.time() - t))
 
-    if epoch > cfg.early_stopping and val_losses[-1] > np.mean(val_losses[-(cfg.early_stopping+1):-1]):
-        print_log("Early stopping...")
-        break
-
-
-print_log("Optimization Finished!")
+#     if epoch > cfg.early_stopping and val_losses[-1] > np.mean(val_losses[-(cfg.early_stopping+1):-1]):
+#         print_log("Early stopping...")
+#         break
 
 
-# Testing
-test_loss, test_acc, pred, labels, test_duration = evaluate(t_features, t_y_test, test_mask)
-print_log("Test set results: \n\t loss= {:.5f}, accuracy= {:.5f}, time= {:.5f}".format(test_loss, test_acc, test_duration))
-
-test_pred = []
-test_labels = []
-for i in range(len(test_mask)):
-    if test_mask[i]:
-        test_pred.append(pred[i])
-        test_labels.append(np.argmax(labels[i]))
+# print_log("Optimization Finished!")
 
 
-print_log("Test Precision, Recall and F1-Score...")
-print_log(metrics.classification_report(test_labels, test_pred, digits=4))
-print_log("Macro average Test Precision, Recall and F1-Score...")
-print_log(metrics.precision_recall_fscore_support(test_labels, test_pred, average='macro'))
-print_log("Micro average Test Precision, Recall and F1-Score...")
-print_log(metrics.precision_recall_fscore_support(test_labels, test_pred, average='micro'))
+# # Testing
+# test_loss, test_acc, pred, labels, test_duration = evaluate(t_features, t_y_test, test_mask)
+# print_log("Test set results: \n\t loss= {:.5f}, accuracy= {:.5f}, time= {:.5f}".format(test_loss, test_acc, test_duration))
+
+# test_pred = []
+# test_labels = []
+# for i in range(len(test_mask)):
+#     if test_mask[i]:
+#         test_pred.append(pred[i])
+#         test_labels.append(np.argmax(labels[i]))
+
+
+# print_log("Test Precision, Recall and F1-Score...")
+# print_log(metrics.classification_report(test_labels, test_pred, digits=4))
+# print_log("Macro average Test Precision, Recall and F1-Score...")
+# print_log(metrics.precision_recall_fscore_support(test_labels, test_pred, average='macro'))
+# print_log("Micro average Test Precision, Recall and F1-Score...")
+# print_log(metrics.precision_recall_fscore_support(test_labels, test_pred, average='micro'))
 
 
 
