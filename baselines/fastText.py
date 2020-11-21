c3180d2c2ec45c6c76e084169ca72de4e67c0413
Dongzi Qu <dq394@nyu.edu>
2020-11-21 15:41:27 -0500

    bigram added

--- baselines/fastText.py
+++ baselines/fastText.py
@@ -1,11 +1,19 @@
 ##-----------------------------------------------------------##
 # Try to use: python fastText.py <dataset> to show performance
 # Results till now:
+#
+# The normal fasttext
 # R8: (2189, 97.12%, 97.12%)
 # R52: (2568, 88.71%, 88.71%)
-# ohsumed: (4043, 30.45%, 30.45%)
+# ohsumed: (4043, 50.45%, 50.45%)
 # THUTC: (600, 92.17%, 92.17%)
 # To be continued...
+#
+# The bigram fasttext
+# R8: (2189, 96.21%, 96.21%)
+# R52: (2568, 85.63%, 85.63%)
+# ohsumed: (4043, 48.29%, 48.29%)
+# THUTC: (600, 89.50%, 89.50%)
 ##-----------------------------------------------------------##
 
 import numpy as np
@@ -69,6 +77,7 @@ generate_fasttext_data(train_df, '_train', dataset)
 generate_fasttext_data(test_df, '_test', dataset)
 print("fastText data generated!")
 
-model = fasttext.train_supervised(input='../cleaned_data/' + dataset + '/fastText/' + dataset + '_train.txt', lr=0.5, epoch=10)
+model = fasttext.train_supervised(input='../cleaned_data/' + dataset + '/fastText/' + dataset + '_train.txt', lr=0.5, epoch=10, wordNgrams=2)
 print("fasttext model trained!")
+print("The Bigram fasttext result:")
 print(model.test('../cleaned_data/' + dataset + '/fastText/' + dataset + '_test.txt'))
